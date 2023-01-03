c37895dd14a7e0fdb5186971a3c315a22f374533
issac <issac.rajan@gmail.com>
2023-01-03 21:03:39 +0530

    init

---
+++ src/main/java/com/issac/memcache/TestMemcacheApplication.java
@@ -0,0 +1,13 @@
+package com.issac.memcache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestMemcacheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestMemcacheApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/issac/memcache/TestMemcacheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.issac.memcache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestMemcacheApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
